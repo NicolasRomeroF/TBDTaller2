3995eb7ac8ca0e2d7437addbf3416913e676cd5b
Maximiliano Espinosa Nelidow <maximiliano.espinosa@usach.cl>
2018-12-03 18:19:27 -0300

    Añadiendo graficos duales para comparación de menciones positivas y negativas

--- plantilla-vue/src/games.vue
+++ plantilla-vue/src/games.vue
@@ -1,12 +1,10 @@
 <template>
     <div class="center">
     
-        <h1>Gráfico donut sobre cantidad de menciones de juegos (Positivas + Negativas)</h1>
-        
-        <br><hr><br>
+        <h1>Gráfico barras sobre cantidad de menciones de juegos (Positivas + Negativas)</h1>
+        <br>
         
         <div style="width:100%; float: left;"  >
-
             <vue-chart  v-if="this.chartData !== null" type="horizontalBar" :data="this.chartData"></vue-chart>
             <div v-else>
                 <div class=" lds-css ng-scope">
@@ -15,8 +13,9 @@
                     </div>
                 </div>
             </div>
-
         </div>
+
+        <br><hr><br>
 
         <div style="width: 75%">
             <vue-chart v-if="this.chartData !== null" type="doughnut" :data="this.chartData"></vue-chart>
@@ -28,6 +27,24 @@
                 </div>
             </div>
         </div>
+
+        <br><hr><br>
+
+        <h1>Gráfico barras comparación de menciones positivas (verde) vs negativas (rojo)</h1>
+
+        <br>
+
+        <div style="width:100%; float: left;"  >
+            <vue-chart  v-if="this.chartDataPosNeg !== null" type="horizontalBar" :data="this.chartDataPosNeg"></vue-chart>
+            <div v-else>
+                <div class=" lds-css ng-scope">
+                    <div style="width:100%;height:100%" class="lds-bars">
+                    <div></div><div></div><div></div><div></div> <div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -68,13 +85,16 @@
                     }
                 ],
                 chartData: null,
+                chartDataPosNeg: null,
                 torta:false,
                 barra: false,
-                colores: []
+                colores: ["#ff001b","#ff7400","#f3ff00","#80ff00","#00ff70","#00ffd8","#00c1ff","#005dff",
+                        "#2e00ff","#d100ff","#ff0080","#ff0000","#ff6800","#fff300","#74ff00"]
             }
         },
         created(){
             this.crearGrafico();
+            this.crearGraficoPositivosYNegativos();
             console.log("grafico creado", this.chartData);
         },
         methods: {
@@ -86,11 +106,6 @@
             mostrarTorta(){
                 this.barra=false;
                 this.torta=true;
-            },
-
-            colorRandom(){
-                var color = "#"+((1<<24)*Math.random()|0).toString(16)
-                this.colores.push(color)
             },
 
             crearGrafico() {
@@ -106,11 +121,35 @@
                 };
 
                 for (let i = 0; i < this.games.length; i++) {
-                    this.colorRandom();
+                    this.colores[i];
                     this.chartData.labels = this.chartData.labels.concat([this.games[i].name]);
                     this.chartData.datasets[0].data.push( this.games[i].val_pos + this.games[i].val_neg);
                 }
-            }
+            },
+
+            crearGraficoPositivosYNegativos(){
+                this.chartDataPosNeg = {
+                    labels: [],
+                    datasets: [
+                        {
+                            label: "Comentarios positivos",
+                            backgroundColor: "#19D610",
+                            data: []
+                        },
+                        {
+                            label: "Comentarios negativos",
+                            backgroundColor: "#FF001B",
+                            data: []
+                        },
+                    ]
+                };
+                for (let i = 0; i < this.games.length; i++) {
+                    this.chartDataPosNeg.labels = this.chartDataPosNeg.labels.concat([
+                    this.games[i].name]);
+                    this.chartDataPosNeg.datasets[0].data.push( this.games[i].val_pos);
+                    this.chartDataPosNeg.datasets[1].data.push( this.games[i].val_neg);
+                }
+            }   
         }
     }
 </script>
